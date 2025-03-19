6c8f95df252e28ab598e98db2f96bce3c8e4252f
David <davidmmyh@gmail.com>
2025-03-19 18:07:49 +0200

    Added top banner slogan and code adjustments

--- src/app/[lang]/(web)/page.tsx
+++ src/app/[lang]/(web)/page.tsx
@@ -10,15 +10,21 @@ import MoreProducts from "@/components/MoreProducts";
 export default function Page() {
   return (
     <>
-      <Image
-        alt="Hero"
-        className="my-6 max-h-96 w-full rounded-lg object-cover object-left-top"
-        height={1080}
-        priority
-        quality={100}
-        src="/images/home-panner.jpg"
-        width={1920}
-      />
+      <div className="relative">
+        <h1 className="absolute left-0 top-0 z-50 p-6 text-5xl font-bold text-primary-foreground">
+          <span>Tomorrow&apos;s Tech,</span>
+          <span className="block mt-2"> Today&apos;s Prices</span>
+        </h1>
+        <Image
+          alt="Hero"
+          className="my-6 max-h-96 w-full rounded-lg object-cover object-left-top"
+          height={1080}
+          priority
+          quality={100}
+          src="/images/home-panner.jpg"
+          width={1920}
+        />
+      </div>
       <div className="space-y-6">
         <FeaturedTags />
         <section className="py-2">
